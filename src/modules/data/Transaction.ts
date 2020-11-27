a1c18598b079d110bfd191a826c9da90342289ee
Michael Kim <michael.kim.200518@gmail.com>
2020-11-27 13:14:36 +0900

    Apply transaction builder and fee calculation to BOAClient

--- src/modules/data/Transaction.ts
+++ src/modules/data/Transaction.ts
@@ -108,36 +108,4 @@ export class Transaction
             elem.computeHash(buffer);
         this.payload.computeHash(buffer);
     }
-
-    /**
-     * Creates a new transaction
-     * @param inputs  An array of 1 or more UTXOs to be spent
-     * @param outputs An array of 1 or more output
-     * @param keys    An array of length matching `inputs` which are the keys controlling the UTXOs
-     * @param payload The data payload to store
-     * @returns The instance of Transaction
-     */
-    public static create (inputs: Array<TxInput>, outputs: Array<TxOutput>, keys: Array<Seed>,
-        payload: DataPayload): Transaction
-    {
-        if (inputs.length == 0)
-            throw new Error ("The number of inputs is 0.");
-
-        if (outputs.length == 0)
-            throw new Error ("The number of outputs is 0.");
-
-        if (inputs.length != keys.length)
-            throw new Error ("The number of inputs and keys are different.");
-
-        let key_pairs: Array<KeyPair> = [];
-        for (let elem of keys)
-            key_pairs.push(KeyPair.fromSeed(elem));
-
-        let tx = new Transaction(TxType.Payment, inputs, outputs, payload);
-        let tx_hash = hashFull(tx);
-        for (let idx = 0; idx < tx.inputs.length; idx++)
-            tx.inputs[idx].signature.data.set(key_pairs[idx].secret.sign(tx_hash.data).data)
-
-        return tx;
-    }
 }
--- src/modules/net/BOAClient.ts
+++ src/modules/net/BOAClient.ts
@@ -14,11 +14,10 @@
 
 *******************************************************************************/
 
-import { DataPayload } from '../data/DataPayload';
-import { Hash, hash, hashFull } from '../data/Hash';
+import { Hash, hash } from '../data/Hash';
 import { Request } from './Request';
 import { Validator } from './response/Validator';
-import { Seed, Transaction, TxInput, TxOutput } from "../..";
+import { Transaction } from "../data/Transaction";
 
 import { AxiosResponse, AxiosError } from 'axios';
 import uri from 'urijs';
@@ -226,50 +225,28 @@ export class BOAClient
 
     /**
      * Saves the data to the blockchain
-     * @param inputs    An array of 1 or more UTXOs to be spent
-     * @param outputs   An array of 1 or more output
-     * @param keys      An array of length matching `inputs` which are the keys controlling the UTXOs
-     * @param payload   The data to store
+     * @param tx The instance of the Transaction
      * @returns Returns true if success, otherwise returns false
      */
-    public saveData (inputs: Array<TxInput>, outputs: Array<TxOutput>, keys: Array<Seed>, payload: DataPayload): Promise<boolean>
+    public sendTransaction (tx: Transaction): Promise<boolean>
     {
         return new Promise<boolean>((resolve, reject) =>
         {
-            try
-            {
-                let tx = Transaction.create(inputs, outputs, keys, payload);
+            let url = uri(this.agora_url)
+                .filename("transaction");
 
-                // TODO: Send a transaction to Agora
-                // ex)
-                /*
-                let url = uri(this.agora_url)
-                    .directory("put_transaction");
-
-                Request.put(url.toString(), tx.toObject())
-                    .then((response: AxiosResponse) =>
-                    {
-                        if (response.status == 200)
-                        {
-                            resolve(true);
-                        }
-                        else
-                        {
-                            // It is not yet defined in Stoa.
-                            reject(new Error(response.statusText));
-                        }
-                    })
-                    .catch((reason: any) =>
-                    {
-                        reject(handleNetworkError(reason));
-                    });
-                */
-
-                resolve(true);
-            } catch (err)
-            {
-                reject(err);
-            }
+            Request.put(url.toString(), {tx: tx})
+                .then((response: AxiosResponse) =>
+                {
+                    if (response.status == 200)
+                        resolve(true);
+                    else
+                        reject(new Error(response.statusText));
+                })
+                .catch((reason: any) =>
+                {
+                    reject(handleNetworkError(reason));
+                });
         });
     }
 }
