9863bafba80aad4481c6818e4d4c4a4100537d4c
Michael Kim <michael.kim.200518@gmail.com>
2021-01-08 16:14:47 +0900

    Add a module Lock.ts that contains the Lock / Unlock definitions
    These are the types that will ultimately replace the `signature` in the `Input` and the `address` in the `Output`.

---
+++ src/modules/script/Lock.ts
@@ -0,0 +1,222 @@
+/*******************************************************************************
+
+    Contains the Lock / Unlock definitions
+
+    These are the types that will ultimately
+    replace the `signature` in the `Input` and the `address` in the `Output`.
+
+    The Lock type contains a tag, allowing 4 different types of lock scripts:
+
+    Key
+        lock is a 64-byte public key,
+        unlock is expected to be a signature.
+
+    KeyHash
+        lock is a hash of a 64-byte public key,
+        unlock is expected to be a pair of <signature, public-key>.
+        This form may be used for better privacy.
+
+    Script
+        lock is a script that will be evaluated
+        by the engine. The unlock script may either
+        be empty or only contain stack push opcodes.
+
+    Redeem
+        lock is a <redeem-hash>, and unlock may
+        only contain stack push opcodes, where the
+        last push will be read as the redeem script.
+
+    Copyright:
+        Copyright (c) 2020 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+import { PublicKey } from '../common/KeyPair';
+import { Signature } from '../common/Signature';
+import { JSONValidator } from '../..';
+
+import { SmartBuffer } from 'smart-buffer';
+
+/**
+ * The input lock types.
+ */
+export enum LockType
+{
+    /// lock is a 64-byte public key, unlock is the signature
+    Key = 0,
+
+    /// lock is a 64-byte public key hash, unlock is a (sig, key) pair
+    KeyHash = 1,
+
+    /// lock is a script, unlock may be anything required by the lock script
+    Script = 2,
+
+    /// lock is a 64-byte hash of a script, unlock is the script containing
+    /// only stack pushes which will push the redeem script last
+    Redeem = 3
+}
+
+/**
+ * Contains a tag and either a Hash or set of opcodes
+ */
+export class Lock
+{
+    /**
+     * Specifies the type of lock script
+     */
+    public type : LockType;
+
+    /**
+     * May either be a Hash, or a sequence of opcodes
+     */
+    public bytes : Buffer;
+
+    /**
+     * Constructor
+     * @param type  Specifies the type of lock script
+     * @param data May either be a Hash, or a sequence of opcodes
+     */
+    constructor (type: LockType, data: Buffer | string)
+    {
+        this.type = type;
+        if (typeof data === 'string')
+            this.bytes = Buffer.from(data, "base64");
+        else
+            this.bytes = Buffer.from(data);
+    }
+
+    /**
+     * The reviver parameter to give to `JSON.parse`
+     * @param key   Name of the field being parsed
+     * @param value The value associated with `key`
+     * @returns A new instance of `Lock` if `key == ""`, `value` otherwise.
+     */
+    public static reviver (key: string, value: any): any
+    {
+        if (key !== "")
+            return value;
+
+        JSONValidator.isValidOtherwiseThrow('Lock', value);
+        return new Lock(value.type, value.bytes);
+    }
+
+    /**
+     * Collects data to create a hash.
+     * @param buffer The buffer where collected data is stored
+     */
+    public computeHash (buffer: SmartBuffer)
+    {
+        buffer.writeUInt8(this.type);
+        buffer.writeBuffer(this.bytes);
+    }
+
+    /**
+     * Generates a LockType.Key lock script.
+     * @param key the public key which can unlock this lock script
+     * @returns The lock script
+     */
+    public static fromPublicKey (key: PublicKey): Lock
+    {
+        return new Lock(LockType.Key, key.data);
+    }
+
+    /**
+     * The bytes consisting of null values for all bytes.
+     * @returns The instance of Unlock
+     */
+    public static get Null (): Lock
+    {
+        return new Lock(LockType.Key, Buffer.alloc(0));
+    }
+
+    /**
+     * Converts this object to its JSON representation
+     */
+    public toJSON (key?: string): any
+    {
+        return {
+            type: this.type,
+            bytes: this.bytes.toString("base64")
+        }
+    }
+}
+
+/**
+ * Contains a data tuple or a set of push opcodes
+ */
+export class Unlock
+{
+    /**
+     * May be: <signature>, <signature, key>, <key, push opcodes>
+     */
+    public bytes : Buffer;
+
+    /**
+     * Constructor
+     * @param data May be: <signature>, <signature, key>, <key, push opcodes>
+     */
+    constructor (data: Buffer | string)
+    {
+        if (typeof data === 'string')
+            this.bytes = Buffer.from(data, "base64");
+        else
+            this.bytes = Buffer.from(data);
+    }
+
+    /**
+     * The reviver parameter to give to `JSON.parse`
+     * @param key   Name of the field being parsed
+     * @param value The value associated with `key`
+     * @returns A new instance of `Unlock` if `key == ""`, `value` otherwise.
+     */
+    public static reviver (key: string, value: any): any
+    {
+        if (key !== "")
+            return value;
+
+        JSONValidator.isValidOtherwiseThrow('Unlock', value);
+        return new Unlock(value.bytes);
+    }
+
+    /**
+     * Collects data to create a hash.
+     * @param buffer The buffer where collected data is stored
+     */
+    public computeHash (buffer: SmartBuffer)
+    {
+        buffer.writeBuffer(this.bytes);
+    }
+
+    /**
+     * Generates a LockType.Key unlock script.
+     * @param sig The signature that will be embedded in the script
+     * @returns The unlock script
+     */
+    public static fromSignature (sig: Signature): Unlock
+    {
+        return new Unlock(sig.data);
+    }
+
+    /**
+     * The bytes consisting of null values for all bytes.
+     * @returns The instance of Unlock
+     */
+    public static get Null (): Unlock
+    {
+        return new Unlock(Buffer.alloc(0));
+    }
+
+    /**
+     * Converts this object to its JSON representation
+     */
+    public toJSON (key?: string): any
+    {
+        return {
+            bytes: this.bytes.toString("base64")
+        }
+    }
+}
