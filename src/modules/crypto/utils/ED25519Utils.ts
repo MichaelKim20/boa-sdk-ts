422970b513c4d3ce36e8721b5dea013b83d61683
Michael Kim <michael.kim.200518@gmail.com>
2021-03-27 23:43:21 +0900

    update10

--- src/modules/crypto/utils/ED25519Utils.ts
+++ src/modules/crypto/utils/ED25519Utils.ts
@@ -8,22 +8,6 @@ export class ED25519Utils
     public static crypto_core_ed25519_SCALARBYTES: number = 32;
     public static crypto_core_ed25519_NONREDUCEDSCALARBYTES: number = 64;
 
-    public static load_3 (s: Uint8Array, offset: number): JSBI
-    {
-        let result = JSBI.BigInt(s[offset]);
-        result = JSBI.bitwiseOr(result, JSBI.leftShift(JSBI.BigInt(s[++offset]), JSBI.BigInt(8)));
-        result = JSBI.bitwiseOr(result, JSBI.leftShift(JSBI.BigInt(s[++offset]), JSBI.BigInt(16)));
-        return result;
-    }
-
-    public static load_4 (s: Uint8Array, offset: number): JSBI
-    {
-        let result = JSBI.BigInt(s[offset]);
-        result = JSBI.bitwiseOr(result, JSBI.leftShift(JSBI.BigInt(s[++offset]), JSBI.BigInt(8)));
-        result = JSBI.bitwiseOr(result, JSBI.leftShift(JSBI.BigInt(s[++offset]), JSBI.BigInt(16)));
-        result = JSBI.bitwiseOr(result, JSBI.leftShift(JSBI.BigInt(s[++offset]), JSBI.BigInt(24)));
-        return result;
-    }
 
     public static sodium_is_zero (n: Uint8Array, len: number): number
     {
