0cf8541933620ae7e5b0d246a2d425b57261ce9a
Michael Kim <michael.kim.200518@gmail.com>
2021-03-29 20:45:17 +0900

    update14

--- src/index.ts
+++ src/index.ts
@@ -95,12 +95,20 @@ export {
 } from './modules/crypto/crypto_core/ed25519/ref10/ed25519_ref10';
 
 export {
-    crypto_core_ed25519_scalar_reduce,
     crypto_core_ed25519_random,
     crypto_core_ed25519_from_uniform,
     crypto_core_ed25519_add,
     crypto_core_ed25519_sub,
-    crypto_core_ed25519_is_valid_point
+    crypto_core_ed25519_is_valid_point,
+    crypto_core_ed25519_scalar_random,
+    crypto_core_ed25519_scalar_add,
+    crypto_core_ed25519_scalar_sub,
+    crypto_core_ed25519_scalar_negate,
+    crypto_core_ed25519_scalar_complement,
+    crypto_core_ed25519_scalar_mul,
+    crypto_core_ed25519_scalar_invert,
+    crypto_core_ed25519_scalar_reduce,
+    crypto_core_ed25519_scalar_is_canonical
 } from './modules/crypto/crypto_core/ed25519/core_ed25519';
 
 export { crypto_sign_ed25519_sk_to_curve25519 } from './modules/crypto/crypto_sign/ed25519/keypair';
