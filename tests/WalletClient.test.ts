ffde53e256e5c5d9ad35f6fcbd17c2cf5ecbd54e
Michael Kim <michael.kim.200518@gmail.com>
2021-10-07 13:58:05 +0900

    Add the test code for WalletUTXOProvider

--- tests/WalletClient.test.ts
+++ tests/WalletClient.test.ts
@@ -286,4 +286,212 @@ describe("Wallet Client", () => {
         assert.deepStrictEqual(third_utxos.length, 1);
         assert.deepStrictEqual(third_utxos[0].utxo.toString(), sample_utxo_client[0].utxo);
     });
+
+    it("Test saving a vote data with `WalletUTXOProvider`", async () => {
+        const wallet_client = new sdk.WalletClient({
+            agoraEndpoint: "http://localhost:6000",
+            stoaEndpoint: "http://localhost:7000",
+            fee: sdk.WalletTransactionFeeOption.Medium,
+        });
+
+        const key_pair = sdk.KeyPair.fromSeed(
+            new sdk.SecretKey("SD4IEXJ6GWZ226ALTDDM72SYMHBTTJ6CHDPUNNTVZK4XSDHAM4BAQIC4")
+        );
+
+        const vote_data = Buffer.from("YXRhZCBldG92", "base64");
+        const payload_fee = sdk.TxPayloadFee.getFee(vote_data.length);
+        const tx_fee = sdk.JSBI.BigInt(0);
+
+        const builder = new sdk.TxBuilder(key_pair);
+
+        // Create UTXOProvider
+        const utxo_provider = new sdk.WalletUTXOProvider(key_pair.address, wallet_client);
+        // Get UTXO for the amount to need.
+        const utxo_res = await utxo_provider.getUTXO(
+            sdk.JSBI.add(sdk.JSBI.add(payload_fee, tx_fee), sdk.JSBI.BigInt(1))
+        );
+        assert.deepStrictEqual(utxo_res.code, sdk.WalletResultCode.Success);
+        assert.ok(utxo_res.data !== undefined);
+        const utxos = utxo_res.data;
+        utxos.forEach((u: sdk.UnspentTxOutput) => builder.addInput(u.utxo, u.amount));
+
+        const expected = {
+            inputs: [
+                {
+                    utxo: "0x3451d94322524e3923fd26f0597fb8a9cdbf3a9427c38ed1ca61104796d39c5b9b5ea33d576f17c2dc17bebc5d84a0559de8c8c521dfe725d4c352255fc71e85",
+                    unlock: {
+                        bytes: "Vh6I8RKAw+8lM0NulP9PotF9DS/+o6cKAPfVNVaZ6QHKg3gM7IOVo3JG5fxw8b1YledAEKqBD/jhQzFVg0LI5w==",
+                    },
+                    unlock_age: 0,
+                },
+                {
+                    utxo: "0x7e1958dbe6839d8520d65013bbc85d36d47a9f64cf608cc66c0d816f0b45f5c8a85a8990725ffbb1ab13c3c65b45fdc06f4745d455e00e1068c4c5c0b661d685",
+                    unlock: {
+                        bytes: "PlOoZA14zITTLDEc7rXmAN7mujRMRYl0y6B0bz3cew+sh0mAT5q0RJBmfRFdyWBjKUaDD1364pHdIaF79Pyhqg==",
+                    },
+                    unlock_age: 0,
+                },
+                {
+                    utxo: "0xfca92fe76629311c6208a49e89cb26f5260777278cd8b272e7bb3021adf429957fd6844eb3b8ff64a1f6074126163fd636877fa92a1f4329c5116873161fbaf8",
+                    unlock: {
+                        bytes: "wu4d7hTwbWTl8i6DHLxLN6ApLZwznOo/1eCowQlI9wzdTHaicUucykDomWv6E8aa31bgawJbGs5kkqKK3zMloA==",
+                    },
+                    unlock_age: 0,
+                },
+            ],
+            outputs: [
+                { type: 0, value: "100000", lock: { type: 0, bytes: "wa1PiNOnmZYBpjfjXS58SZ6fJTaihHSRZRt86aWWRgE=" } },
+            ],
+            payload: "YXRhZCBldG92",
+            lock_height: "0",
+        };
+        const tx = builder.assignPayload(vote_data).sign(sdk.OutputType.Payment, tx_fee, payload_fee);
+
+        // Because randomly generated values are used when signing,
+        // different signatures are created even when signed using the same secret key.
+        // Therefore, omit the signature comparison.
+        tx.inputs.forEach((value, idx) => {
+            expected.inputs[idx].unlock = value.unlock.toJSON();
+        });
+
+        assert.strictEqual(JSON.stringify(tx), JSON.stringify(expected));
+
+        const res = await wallet_client.sendTransaction(tx);
+        assert.deepStrictEqual(res.code, sdk.WalletResultCode.Success);
+        assert.ok(res.data !== undefined);
+        assert.deepStrictEqual(res.data, true);
+    });
+
+    it("Test saving a vote data with `WalletUTXOProvider` - There is no output", async () => {
+        const wallet_client = new sdk.WalletClient({
+            agoraEndpoint: URI("http://localhost").port(agora_port).toString(),
+            stoaEndpoint: URI("http://localhost").port(stoa_port).toString(),
+            fee: sdk.WalletTransactionFeeOption.Medium,
+        });
+
+        const key_pair = sdk.KeyPair.fromSeed(
+            new sdk.SecretKey("SD4IEXJ6GWZ226ALTDDM72SYMHBTTJ6CHDPUNNTVZK4XSDHAM4BAQIC4")
+        );
+
+        const vote_data = Buffer.from("YXRhZCBldG92", "base64");
+        const payload_fee = sdk.JSBI.BigInt(200000);
+        const tx_fee = sdk.JSBI.BigInt(0);
+
+        const builder = new sdk.TxBuilder(key_pair);
+
+        // Create UTXOProvider
+        const utxo_provider = new sdk.WalletUTXOProvider(key_pair.address, wallet_client);
+        // Get UTXO for the amount to need.
+        // There can't be any output. An error occurs because the constraint of
+        // the transaction is not satisfied that it must have at least one output.
+        const utxo_res = await utxo_provider.getUTXO(sdk.JSBI.add(payload_fee, tx_fee));
+        assert.deepStrictEqual(utxo_res.code, sdk.WalletResultCode.Success);
+        assert.ok(utxo_res.data !== undefined);
+        const utxos = utxo_res.data;
+        utxos.forEach((u: sdk.UnspentTxOutput) => builder.addInput(u.utxo, u.amount));
+
+        assert.throws(() => {
+            const tx = builder.assignPayload(vote_data).sign(sdk.OutputType.Payment, tx_fee, payload_fee);
+        });
+    });
+
+    it("Test saving a vote data - There is at least one output", async () => {
+        const wallet_client = new sdk.WalletClient({
+            agoraEndpoint: URI("http://localhost").port(agora_port).toString(),
+            stoaEndpoint: URI("http://localhost").port(stoa_port).toString(),
+            fee: sdk.WalletTransactionFeeOption.Medium,
+        });
+
+        const key_pair = sdk.KeyPair.fromSeed(
+            new sdk.SecretKey("SD4IEXJ6GWZ226ALTDDM72SYMHBTTJ6CHDPUNNTVZK4XSDHAM4BAQIC4")
+        );
+
+        const vote_data = Buffer.from("YXRhZCBldG92", "base64");
+        const payload_fee = sdk.JSBI.BigInt(200000);
+        const tx_fee = sdk.JSBI.BigInt(0);
+
+        const builder = new sdk.TxBuilder(key_pair);
+
+        // Create UTXOProvider
+        const utxo_provider = new sdk.WalletUTXOProvider(key_pair.address, wallet_client);
+        // Get UTXO for the amount to need.
+        // The amount of the UTXO found is one greater than the fee, allowing at least one change output.
+        const utxo_res = await utxo_provider.getUTXO(
+            sdk.JSBI.add(sdk.JSBI.add(payload_fee, tx_fee), sdk.JSBI.BigInt(1))
+        );
+        assert.deepStrictEqual(utxo_res.code, sdk.WalletResultCode.Success);
+        assert.ok(utxo_res.data !== undefined);
+        const utxos = utxo_res.data;
+        utxos.forEach((u: sdk.UnspentTxOutput) => builder.addInput(u.utxo, u.amount));
+
+        const tx = builder.assignPayload(vote_data).sign(sdk.OutputType.Payment, tx_fee, payload_fee);
+
+        const expected = {
+            inputs: [
+                {
+                    utxo: "0x3451d94322524e3923fd26f0597fb8a9cdbf3a9427c38ed1ca61104796d39c5b9b5ea33d576f17c2dc17bebc5d84a0559de8c8c521dfe725d4c352255fc71e85",
+                    unlock: {
+                        bytes: "EEAMWbm0H0MkmB+FwbP1IvUmvPV1GR5THFECDg5C2APUfF9/SDEstKEmZWS2zJfh4PFXAzmpsiNrSOyuM2y6gw==",
+                    },
+                    unlock_age: 0,
+                },
+                {
+                    utxo: "0xfca92fe76629311c6208a49e89cb26f5260777278cd8b272e7bb3021adf429957fd6844eb3b8ff64a1f6074126163fd636877fa92a1f4329c5116873161fbaf8",
+                    unlock: {
+                        bytes: "7pgvYc4SRBFAvPfD6Y9Ee+juGEvl9aXt7+UlkO2iWQfpYLL9HNZCH5PwslrCv3MvLpZH6H+kUYTozwSl2hg4lg==",
+                    },
+                    unlock_age: 0,
+                },
+            ],
+            outputs: [
+                { type: 0, value: "200000", lock: { type: 0, bytes: "wa1PiNOnmZYBpjfjXS58SZ6fJTaihHSRZRt86aWWRgE=" } },
+            ],
+            payload: "YXRhZCBldG92",
+            lock_height: "0",
+        };
+        // Because randomly generated values are used when signing,
+        // different signatures are created even when signed using the same secret key.
+        // Therefore, omit the signature comparison.
+        tx.inputs.forEach((value, idx) => {
+            expected.inputs[idx].unlock = value.unlock.toJSON();
+        });
+
+        assert.strictEqual(JSON.stringify(tx), JSON.stringify(expected));
+
+        const res = await wallet_client.sendTransaction(tx);
+        assert.deepStrictEqual(res.code, sdk.WalletResultCode.Success);
+        assert.ok(res.data !== undefined);
+        assert.deepStrictEqual(res.data, true);
+    });
+
+    it("Test the WalletUTXOProvider", async () => {
+        const wallet_client = new sdk.WalletClient({
+            agoraEndpoint: URI("http://localhost").port(agora_port).toString(),
+            stoaEndpoint: URI("http://localhost").port(stoa_port).toString(),
+            fee: sdk.WalletTransactionFeeOption.Medium,
+        });
+
+        const public_key = new sdk.PublicKey("boa1xrq66nug6wnen9sp5cm7xhfw03yea8e9x63ggay3v5dhe6d9jerqz50eld0");
+
+        const utxoProvider = new sdk.WalletUTXOProvider(public_key, wallet_client);
+        // Request First UTXO
+        const first_res = await utxoProvider.getUTXO(sdk.JSBI.BigInt(300000), sdk.JSBI.BigInt(100000));
+        assert.deepStrictEqual(first_res.code, sdk.WalletResultCode.Success);
+        assert.ok(first_res.data !== undefined);
+        const first_utxos = first_res.data;
+        assert.deepStrictEqual(first_utxos.length, 3);
+        assert.deepStrictEqual(first_utxos[0].utxo.toString(), sample_utxo_client[1].utxo);
+        assert.deepStrictEqual(first_utxos[1].utxo.toString(), sample_utxo_client[2].utxo);
+        assert.deepStrictEqual(first_utxos[2].utxo.toString(), sample_utxo_client[3].utxo);
+
+        // Request Second UTXO
+        const second_res = await utxoProvider.getUTXO(sdk.JSBI.BigInt(300000), sdk.JSBI.BigInt(100000));
+        assert.deepStrictEqual(second_res.code, sdk.WalletResultCode.Success);
+        assert.ok(second_res.data !== undefined);
+        const second_utxos = second_res.data;
+        assert.deepStrictEqual(second_utxos.length, 3);
+        assert.deepStrictEqual(second_utxos[0].utxo.toString(), sample_utxo_client[4].utxo);
+        assert.deepStrictEqual(second_utxos[1].utxo.toString(), sample_utxo_client[5].utxo);
+        assert.deepStrictEqual(second_utxos[2].utxo.toString(), sample_utxo_client[6].utxo);
+    });
 });
